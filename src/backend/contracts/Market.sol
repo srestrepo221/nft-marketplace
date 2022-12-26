2fe59451f83225792faacf66b9eae0745f9143c3
santiago restrepo <srest031@fiu.edu>
2022-12-25 20:32:27 -0500

    Updated Market.sol & Test Script

--- src/backend/contracts/Market.sol
+++ src/backend/contracts/Market.sol
@@ -12,10 +12,54 @@ contract Market is ReentrancyGuard {
 	// State variables
 	address payable public immutable feeAccount; // the account that receives fees
 	uint public immutable feePercent; // the fee percentage on sales
+	uint public itemCount;
+
+	struct Item{
+		uint itemId; 
+		IERC721 nft;
+		uint tokenId;
+		uint price;
+		address payable seller;
+		bool sold;
+	}
+	event Offered(
+		uint itemId,
+		address indexed nft,
+		uint tokenId,
+		uint price,
+		address indexed seller
+	);
+
+	// itemId -> Item
+	mapping(uint => Item) public items; 
 
 	constructor(uint _feePercent) {
 		feeAccount = payable(msg.sender);
 		feePercent = _feePercent;
 	}
-}
 
+	function makeItem(IERC721 _nft, uint _tokenId, uint _price) external nonReentrant {
+		require(_price > 0, "Price must be greater than zero");
+		// Increment itemCount
+		itemCount ++; 
+		// tranfer nft
+		_nft.transferFrom(msg.sender, address(this), _tokenId);
+		// add new items to items mapping
+		items[itemCount] = Item (
+			itemCount,
+			_nft,
+			_tokenId,
+			_price,
+			payable(msg.sender),
+			false
+		);
+		// emit Offered event
+		emit Offered(
+			itemCount,
+			address(_nft),
+			_tokenId,
+			_price,
+			msg.sender
+		);
+	}
+}
--- src/backend/test/NFTMarket.test.js
+++ src/backend/test/NFTMarket.test.js
@@ -1,8 +1,12 @@
 const { expect } =  require("chai");
 
-describe("NFTMarket",  function() {
+const toWei = (num) => ethers.utils.parseEther(num.toString()) // 1 ether = 10**18 wei
+const fromWei = (num) => ethers.utils.formatEther(num)
+
+describe("NFT-Marketplace",  function() {
 	let deployer, addr1, addr2, nft, market
 	let feePercent = 1
+	let URI = "Sample URI"
 	beforeEach(async function() {
 		// Get contract factories 
 		const NFT = await ethers.getContractFactory("NFT");
@@ -12,11 +16,60 @@ describe("NFTMarket",  function() {
 		// Deploy contracts
 		nft = await NFT.deploy();
 		market = await Market.deploy(feePercent);
-	});
+	})
 	describe("Deployment", function() {
 		it("should track name and symbol of the nft contract", async function(){
 			expect(await nft.name()).to.equal("GreenBros NFT")
 			expect(await nft.symbol()).to.equal("GB")
+		})
+		it("should track feeAccount and feePercent of the NFT-Marketplace", async function() {
+			expect(await market.feeAccount()).to.equal(deployer.address)
+			expect(await market.feePercent()).to.equal(feePercent)
+		})
+	})
+	describe("Minting NFTs", function() {
+		it("should track each minted NFT", async function(){
+			// addr 1 mints an nft
+			await nft.connect(addr1).mint(URI)
+			expect(await nft.tokenCount()).to.equal(1);
+			expect(await nft.balanceOf(addr1.address)).to.equal(1);
+			expect(await nft.tokenURI(1)).to.equal(URI)
+			// addr 2 mints an nft
+			await nft.connect(addr2).mint(URI)
+			expect(await nft.tokenCount()).to.equal(2);
+			expect(await nft.balanceOf(addr2.address)).to.equal(1);
+			expect(await nft.tokenURI(2)).to.equal(URI)
+		})
+	})
+	describe("Making marketplace items", function() {
+		beforeEach(async function () {
+			// addr1 mints an nft
+			await nft.connect(addr1).mint(URI)
+			// addr1 approves marketplace to spend nft
+			await nft.connect(addr1).setApprovalForAll(market.address, true)
+		})
+		it("Should track newly created item, transfer NFT from seller to marketplace and emit Offered event", async function () {
+			// addr1 offers their nft at a price of 1 ether
+			await expect(market.connect(addr1).makeItem(nft.address, 1, toWei(1)))
+			.to.emit(market, "Offered")
+				.withArgs(
+					1,
+					nft.address,
+					1,
+					toWei(1),
+					addr1.address
+				)
+			// Owner of NFT should now be the marketplace
+			expect(await nft.ownerOf(1)).to.equal(market.address);
+			// Item count should now equal 1
+			expect(await market.itemCount()).to.equal(1)
+			// Get item from items mapping then check fields to ensure accurate
+			const item = await market.items(1)
+			expect(item.itemId).to.equal(1)
+			expect(item.nft).to.equal(nft.address)
+			expect(item.tokenId).to.equal(1)
+			expect(item.price).to.equal(toWei(1))
+			expect(item.sold).to.equal(false)
 		})
 	})
 })
