59951d729e0bc946dbb28ad05c0c248d07469f96
santiago restrepo <srest031@fiu.edu>
2022-12-26 22:58:53 -0500

    Updated FrontEnd App.js

--- src/frontend/components/App.js
+++ src/frontend/components/App.js
@@ -1,49 +1,46 @@
+import {
+    Link
+} from "react-router-dom";
+import { Navbar, Nav, Button, Container } from 'react-bootstrap'
+import market from './market.png'
 
-import logo from './logo.png';
-import './App.css';
- 
-function App() {
-  return (
-    <div>
-      <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-        <a
-          className="navbar-brand col-sm-3 col-md-2 ms-3"
-          href="http://www.dappuniversity.com/bootcamp"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Dapp University
-        </a>
-      </nav>
-      <div className="container-fluid mt-5">
-        <div className="row">
-          <main role="main" className="col-lg-12 d-flex text-center">
-            <div className="content mx-auto mt-5">
-              <a
-                href="http://www.dappuniversity.com/bootcamp"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <img src={logo} className="App-logo" alt="logo"/>
-              </a>
-              <h1 className= "mt-5">Dapp University Starter Kit</h1>
-              <p>
-                Edit <code>src/frontend/components/App.js</code> and save to reload.
-              </p>
-              <a
-                className="App-link"
-                href="http://www.dappuniversity.com/bootcamp"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                LEARN BLOCKCHAIN <u><b>NOW! </b></u>
-              </a>
-            </div>
-          </main>
-        </div>
-      </div>
-    </div>
-  );
+const Navigation = ({ web3Handler, account }) => {
+    return (
+        <Navbar expand="lg" bg="secondary" variant="dark">
+            <Container>
+                <Navbar.Brand href="http://www.dappuniversity.com/bootcamp">
+                    <img src={market} width="40" height="40" className="" alt="" />
+                    &nbsp; DApp NFT Marketplace
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link as={Link} to="/">Home</Nav.Link>
+                        <Nav.Link as={Link} to="/create">Create</Nav.Link>
+                        <Nav.Link as={Link} to="/my-listed-items">My Listed Items</Nav.Link>
+                        <Nav.Link as={Link} to="/my-purchases">My Purchases</Nav.Link>
+                    </Nav>
+                    <Nav>
+                        {account ? (
+                            <Nav.Link
+                                href={`https://etherscan.io/address/${account}`}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className="button nav-button btn-sm mx-4">
+                                <Button variant="outline-light">
+                                    {account.slice(0, 5) + '...' + account.slice(38, 42)}
+                                </Button>
+
+                            </Nav.Link>
+                        ) : (
+                            <Button onClick={web3Handler} variant="outline-light">Connect Wallet</Button>
+                        )}
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    )
+
 }
 
-export default App;
+export default Navigation;
