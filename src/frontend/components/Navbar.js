40f30c97852fe8de901bc02691d7280e446cee2b
santiago restrepo <srest031@fiu.edu>
2022-12-29 22:07:56 -0500

    Completed Create.js

--- src/frontend/components/Navbar.js
+++ src/frontend/components/Navbar.js
@@ -8,7 +8,7 @@ const Navigation = ({ web3Handler, account }) => {
             <Container>
                 <Navbar.Brand href="http://www.dappuniversity.com/bootcamp">
                     <img src={market} width="40" height="40" className="" alt="" />
-                    &nbsp; DApp NFT Marketplace
+                    &nbsp; GreenBros NFT Marketplace
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
